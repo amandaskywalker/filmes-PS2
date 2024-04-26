682bf0c19c1a9d8bf1da99f7ff6b4a8099008d19
Amanda Massari <54778522+amandaskywalker@users.noreply.github.com>
2024-04-26 01:24:21 +0000

    filme

---
+++ Filme.java
@@ -0,0 +1,54 @@
+package ps2.restapidb;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="filmes")
+
+public class Filme {
+    private Long id;
+    private String titulo;
+    private String diretor;
+    private int anoLancamento;
+    private String genero;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        retur titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void getAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
