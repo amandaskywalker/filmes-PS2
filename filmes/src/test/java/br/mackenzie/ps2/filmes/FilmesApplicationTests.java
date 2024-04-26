305d661634fef9cb1dd7987eb62f018bac9beaec
Amanda Massari <54778522+amandaskywalker@users.noreply.github.com>
2024-04-26 01:35:13 +0000

    filmes

---
+++ filmes/src/test/java/br/mackenzie/ps2/filmes/FilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ps2.filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
