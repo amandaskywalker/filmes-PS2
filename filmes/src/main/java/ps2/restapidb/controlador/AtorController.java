d227ce12ac8b976209701b54fe4a15f7ac4c21f2
Amanda Massari <54778522+amandaskywalker@users.noreply.github.com>
2024-05-22 22:26:40 +0000

    ator

--- filmes/src/main/java/ps2/restapidb/controlador/AtorController.java
+++ filmes/src/main/java/ps2/restapidb/controlador/AtorController.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,6 +19,7 @@ import ps2.restapidb.entidade.Ator;
 import ps2.restapidb.repositorio.AtorRepo;
 
 @RestController
+@RequestMapping("/atores")
 public class AtorController {
 
     @Autowired
@@ -25,37 +28,51 @@ public class AtorController {
     public AtorController() {
     }
 
-    @GetMapping("/atores")
-    Iterable<Ator> getAtores() {
+    @GetMapping
+    public Iterable<Ator> getAtores() {
         return atorRepo.findAll();
     }
 
-    @GetMapping("/atores/{id}")
-    Optional<Ator> getAtor(@PathVariable long id) {
-        return atorRepo.findById(id);
-    }
-
-    @PostMapping("/create/atores")
-    Ator createAtor(@RequestBody Ator t) {
-        Ator createdAtor = atorRepo.save(t);
-        return createdAtor;
-    }
-
-    @PutMapping("/update/atores/{atorId}")
-    Optional<Ator> updateAtor(@RequestBody Ator atorRequest, @PathVariable long atorId) {
-        Optional<Ator> opt = atorRepo.findById(atorId);
-        if (opt.isPresent()) {
-            if (atorRequest.getId() == atorId) {
-                atorRepo.save(atorRequest);
-                return opt;
-            }
+    @GetMapping("/{id}")
+    public ResponseEntity<Ator> getAtor(@PathVariable long id) {
+        Optional<Ator> ator = atorRepo.findById(id);
+        if (ator.isPresent()) {
+            return ResponseEntity.ok(ator.get());
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Erro ao alterar dados do Ator com id " + atorId);
     }
 
-    @DeleteMapping("/delete/atores/{id}")
-    void deleteAtor(@PathVariable long id) {
-        atorRepo.deleteById(id);
+    @PostMapping
+    public Ator createAtor(@RequestBody Ator t) {
+        return atorRepo.save(t);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ator> updateAtor(@RequestBody Ator atorRequest, @PathVariable long id) {
+        Optional<Ator> opt = atorRepo.findById(id);
+        if (opt.isPresent()) {
+            if (atorRequest.getId().equals(id)) {
+                atorRepo.save(atorRequest);
+                return ResponseEntity.ok(opt.get());
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "ID do corpo da solicitação não corresponde ao ID do caminho");
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Erro ao alterar dados do Ator com id " + id);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAtor(@PathVariable long id) {
+        Optional<Ator> opt = atorRepo.findById(id);
+        if (opt.isPresent()) {
+            atorRepo.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- filmes/src/main/java/ps2/restapidb/repositorio/AtorRepo.java
+++ filmes/src/main/java/ps2/restapidb/repositorio/AtorRepo.java
@@ -1,5 +1,9 @@
 package ps2.restapidb.repositorio;
 
-public class AtorRepo {
-    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ps2.restapidb.entidade.Ator;
+
+@Repository
+public interface AtorRepo extends CrudRepository<Ator, Long> {
 }
